4b276ce6b95e91717c0a99f8c6488063db90167d
Rohit Multani <rohitmultani@153.com>
2024-06-03 00:44:33 +0530

    zoomfixed

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -170,7 +170,6 @@ const Map = () => {
         {visitedAnimationComplete && pathPositions.length > 0 && (
           <AnimatedPolyline positions={pathPositions} color="red" />
         )}
-        <ZoomControl position="bottomleft" />
       </MapContainer>
     </>
   );
